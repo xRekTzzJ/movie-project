1edcd1dc85f0fc185c22c55dec3f50751b8f2f19
Pavel Popov <pavel2003sbk@yandex.ru>
2024-03-10 14:20:28 +0800

    сделал шаблон карточки

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,39 @@
-import { DatePicker } from 'antd';
+import { Card, Flex, Rate, Typography } from 'antd';
 import React, { Component } from 'react';
+
+import logo from '../../image/logo.png';
 export default class App extends Component {
   render() {
     return (
       <section className="page">
-        <DatePicker />
+        <Card>
+          <Flex gap={20}>
+            <img alt="avatar" src={logo} className="card__image" />
+            <Flex vertical="true" gap={7}>
+              <Flex align="center" justify="space-between">
+                <Typography.Text className="card__title">The way back</Typography.Text>
+                <Typography.Text className="card__score">6.6</Typography.Text>
+              </Flex>
+              <Typography.Text className="card__date">March 5, 2020</Typography.Text>
+              <Flex align="start" gap={8}>
+                <Typography.Text className="card__genre">Action</Typography.Text>
+                <Typography.Text className="card__genre">Drama</Typography.Text>
+              </Flex>
+              <Typography.Paragraph
+                className="card__description"
+                ellipsis={{
+                  rows: 6,
+                }}
+              >
+                A former basketball all-star, who has lost his wife and family foundation in a struggle with addiction
+                attempts to regain his soul and salvation by becoming the coach of a disparate ethnically mixed high. A
+                former basketball all-star, who has lost his wife and family foundation in a struggle with addiction
+                attempts to regain his soul and salvation by becoming the coach of a disparate ethnically mixed high
+              </Typography.Paragraph>
+              <Rate allowHalf={true} count={10} />
+            </Flex>
+          </Flex>
+        </Card>
       </section>
     );
   }
