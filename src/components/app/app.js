8025c7d44afdb77b6a1e7f44ddece387b8a233f7
Pavel Popov <pavel2003sbk@yandex.ru>
2024-03-10 12:11:29 +0800

    подключил библиотеки и подготовил проект к разработке

---
+++ src/components/app/app.js
@@ -0,0 +1,11 @@
+import { DatePicker } from 'antd';
+import React, { Component } from 'react';
+export default class App extends Component {
+  render() {
+    return (
+      <section className="page">
+        <DatePicker />
+      </section>
+    );
+  }
+}
