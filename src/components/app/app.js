5797e1c7035e4025f43b2f308432af7e7d3bcfa8
Pavel Popov <pavel2003sbk@yandex.ru>
2024-03-11 14:09:42 +0800

    подключил алерт об ошибке

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,5 @@
 import { LoadingOutlined } from '@ant-design/icons';
-import { Input, Pagination, Spin } from 'antd';
+import { Alert, Input, Pagination, Spin } from 'antd';
 import React, { Component } from 'react';
 
 import CardList from '../cardList';
@@ -16,44 +16,63 @@ export default class App extends Component {
     name: null,
     isRatedList: false,
     loading: true,
+    error: false,
   };
 
   getMovies = (name, page = 1) => {
     this.setState({
       loading: true,
     });
-    this.movie.getMovies(name, page).then((res) =>
-      this.setState({
-        movies: res.results,
-        name,
-        page: res.page,
-        totalPages: res.total_pages,
-        isRatedList: false,
-        loading: false,
-      })
-    );
+    this.movie
+      .getMovies(name, page)
+      .then((res) =>
+        this.setState({
+          movies: res.results,
+          name,
+          page: res.page,
+          totalPages: res.total_pages,
+          isRatedList: false,
+          loading: false,
+          error: false,
+        })
+      )
+      .catch(() =>
+        this.setState({
+          error: true,
+          loading: false,
+        })
+      );
   };
   getRatedMovies = (page = 1) => {
     this.setState({
       loading: true,
     });
-    this.movie.getTrendMovies(page).then((res) =>
-      this.setState({
-        movies: res.results,
-        name: null,
-        page: res.page,
-        totalPages: res.total_pages,
-        isRatedList: true,
-        loading: false,
-      })
-    );
+    this.movie
+      .getRatedMovies(page)
+      .then((res) =>
+        this.setState({
+          movies: res.results,
+          name: null,
+          page: res.page,
+          totalPages: res.total_pages,
+          isRatedList: true,
+          loading: false,
+          error: false,
+        })
+      )
+      .catch(() =>
+        this.setState({
+          error: true,
+          loading: false,
+        })
+      );
   };
   onHeaderButtonClick = (e) => {
     e.key === '1' ? this.getMovies('The') : this.getRatedMovies();
   };
   movie = new MovieService();
   render() {
-    const { movies, page, totalPages, isRatedList, name, loading } = this.state;
+    const { movies, page, totalPages, isRatedList, name, loading, error } = this.state;
     return (
       <section className="page">
         <Header onHeaderButtonClick={this.onHeaderButtonClick} />
@@ -71,13 +90,17 @@ export default class App extends Component {
             }
           />
         ) : null}
-        {!loading ? <CardList movies={movies} /> : null}
+        {error ? (
+          <Alert showIcon type="error" description="The error is on the server side. Please try again later." />
+        ) : null}
+        {!loading && !error ? <CardList movies={movies} /> : null}
         <Pagination
           className="pagination"
           defaultCurrent={page}
           current={page}
           total={totalPages}
           showSizeChanger={false}
+          disabled={error ? true : false}
           onChange={(page) => {
             isRatedList ? this.getRatedMovies(page) : this.getMovies(name, page);
           }}
--- src/components/services/movie-service.js
+++ src/components/services/movie-service.js
@@ -7,7 +7,7 @@ export default class MovieService {
         'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOWJlM2UzYWJmYWNmYTM3NDhjYjg1MjA3MjNmZDY3NCIsInN1YiI6IjY1ZWQ1ZmFiNDQ3ZjljMDE2NDVmOTQzMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._sDORt3oFzSRlsqWyW-h6uQL_gs0bWG1nMaXd_oxf4E',
     },
   };
-  async getTrendMovies(page = 1) {
+  async getRatedMovies(page = 1) {
     const res = await fetch(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${page}`, this.options);
     return res.json();
   }
